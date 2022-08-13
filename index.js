d7e8021f6637be27aa3001f864ecfd4b2e344fd2
asunguyen <cntt510@gmail.com>
2022-08-14 05:52:23 +0700

    update

--- index.js
+++ index.js
@@ -38,15 +38,18 @@ app.listen(PORT, () => console.log("start serve"))
 
 let readXlsxFile = require('read-excel-file/node');
 let arrData;
-var getDataExcel = async function() {
+let totalRowsCard;
+let totalRowsVoucher5;
+let totalRowsVoucher10;
+var getDataExcel = async function () {
     console.log(1);
-    let totalRowsCard = await readXlsxFile('public/DATA_UP_220808.xlsx', { sheet: "Card" }).then((rows) => {
+    totalRowsCard = await readXlsxFile('public/DATA_UP_220808.xlsx', { sheet: "Card" }).then((rows) => {
         return rows;
     });
-    let totalRowsVoucher5 = await readXlsxFile('public/DATA_UP_220808.xlsx', { sheet: "Voucher 5%" }).then((rows) => {
+    totalRowsVoucher5 = await readXlsxFile('public/DATA_UP_220808.xlsx', { sheet: "Voucher 5%" }).then((rows) => {
         return rows;
     });
-    let totalRowsVoucher10 = await readXlsxFile('public/DATA_UP_220808.xlsx', { sheet: "Voucher 10%" }).then((rows) => {
+    totalRowsVoucher10 = await readXlsxFile('public/DATA_UP_220808.xlsx', { sheet: "Voucher 10%" }).then((rows) => {
         return rows;
     });
     arrData = totalRowsCard.concat(totalRowsVoucher5, totalRowsVoucher10);
@@ -56,69 +59,118 @@ getDataExcel();
 
 
 app.get("/", (req, res) => {
-    
+
     res.render("trangchu");
 });
 app.post("/read-file", async (req, res) => {
-    //[Voucher 5%, Card, Voucher 10%]
-
-
-    //res.json({code: 200, data: "arrData"});
-
-    //
-    
-    const dataCount = await Count.findOne();
-    let number = 0;
-    if (dataCount) {
-        number = dataCount.countdata;
-    }
-    if (arrData && arrData[number] && arrData[number][1] && arrData[number][1] != "Mã Khách Hàng") {
-        const newUser = new User({
-            phone: arrData[number][3],
-            name: arrData[number][2],
-            phanthuong: arrData[number][5],
-            ma_kh: arrData[number][1],
-            mid: arrData[number][3],
-            khuvuc: arrData[number][4],
-            province: arrData[number][4],
-            voucher: arrData[number][6],
-            userCustom: "true"
-        });
-        await newUser.save();
-        number++;
+    try {
+        const dataCount = await Count.findOne();
+        let number = 0;
+        let numberCard = 0;
+        let number5 = 0;
+        let number10 = 0;
         if (dataCount) {
-            await Count.findByIdAndUpdate(dataCount._id, { countdata: number });
-        } else {
-            const newCount = new Count({
-                countdata: number
-            });
-            await newCount.save();
+            number = dataCount.countdata;
+            numberCard = dataCount.countCard;
+            number5 = dataCount.count5;
+            number10 = dataCount.count10;
         }
-        
-        if (number < arrData.length) {
-            res.json({ code: 200, user: newUser });
-        } else {
-            res.json({ code: 200, user: newUser, done: "true" });
+        //push card
+        if (number % 9 == 0) {
+            if (totalRowsCard && totalRowsCard[numberCard] && totalRowsCard[numberCard][1] && totalRowsCard[numberCard][1] != "Mã Khách Hàng") {
+                const newUser = new User({
+                    phone: totalRowsCard[numberCard][3],
+                    name: totalRowsCard[numberCard][2],
+                    phanthuong: totalRowsCard[numberCard][5],
+                    ma_kh: totalRowsCard[numberCard][1],
+                    mid: totalRowsCard[numberCard][3],
+                    khuvuc: totalRowsCard[numberCard][4],
+                    province: totalRowsCard[numberCard][4],
+                    voucher: totalRowsCard[numberCard][6],
+                    userCustom: "true"
+                });
+                await newUser.save();
+                numberCard++;
+                number++
+                await Count.findByIdAndUpdate(dataCount._id, { count10: number10, count5: number5, countdata: number, countCard: numberCard });
+                res.json({ code: 200, user: newUser });
+            } else {
+                number++;
+                numberCard++;
+                await Count.findByIdAndUpdate(dataCount._id, { count10: number10, count5: number5, countdata: number, countCard: numberCard });
+                if (number < arrData.length) {
+                    res.json({ code: 200, user: {} });
+                } else {
+                    res.json({ code: 200, user: {}, done: "true" });
+                }
+
+            }
         }
-    } else {
-        number++;
-        if (dataCount) {
-            await Count.findByIdAndUpdate(dataCount._id, { countdata: number });
-        } else {
-            const newCount = new Count({
-                countdata: number
-            });
-            await newCount.save();
+        // push voucher 5%
+        if (number % 9 == 1 || number % 9 == 2 || number % 9 == 3 || number % 9 == 7) {
+            if (totalRowsVoucher5 && totalRowsVoucher5[number5] && totalRowsVoucher5[number5][1] && totalRowsVoucher5[number5][1] != "Mã Khách Hàng") {
+                const newUser = new User({
+                    phone: totalRowsVoucher5[number5][3],
+                    name: totalRowsVoucher5[number5][2],
+                    phanthuong: totalRowsVoucher5[number5][5],
+                    ma_kh: totalRowsVoucher5[number5][1],
+                    mid: totalRowsVoucher5[number5][3],
+                    khuvuc: totalRowsVoucher5[number5][4],
+                    province: totalRowsVoucher5[number5][4],
+                    voucher: totalRowsVoucher5[number5][6],
+                    userCustom: "true"
+                });
+                await newUser.save();
+                number5++;
+                number++
+                await Count.findByIdAndUpdate(dataCount._id, { count10: number10, count5: number5, countdata: number, countCard: numberCard });
+                res.json({ code: 200, user: newUser });
+            } else {
+                number++;
+                number5++;
+                await Count.findByIdAndUpdate(dataCount._id, { count10: number10, count5: number5, countdata: number, countCard: numberCard });
+                if (number < arrData.length) {
+                    res.json({ code: 200, user: {} });
+                } else {
+                    res.json({ code: 200, user: {}, done: "true" });
+                }
+
+            }
         }
-        if (number < arrData.length) {
-            res.json({ code: 200, user: {} });
-        } else {
-            res.json({ code: 200, user: {}, done: "true" });
+        //push voucher 10
+        if (number % 9 == 4 || number % 9 == 5 || number % 9 == 6 || number % 9 == 8) {
+            if (totalRowsVoucher10 && totalRowsVoucher10[number10] && totalRowsVoucher10[number10][1] && totalRowsVoucher10[number10][1] != "Mã Khách Hàng") {
+                const newUser = new User({
+                    phone: totalRowsVoucher10[number10][3],
+                    name: totalRowsVoucher10[number10][2],
+                    phanthuong: totalRowsVoucher10[number10][5],
+                    ma_kh: totalRowsVoucher10[number10][1],
+                    mid: totalRowsVoucher10[number10][3],
+                    khuvuc: totalRowsVoucher10[number10][4],
+                    province: totalRowsVoucher10[number10][4],
+                    voucher: totalRowsVoucher10[number10][6],
+                    userCustom: "true"
+                });
+                await newUser.save();
+                number10++;
+                number++
+                await Count.findByIdAndUpdate(dataCount._id, { count10: number10, count5: number5, countdata: number, countCard: numberCard });
+                res.json({ code: 200, user: newUser });
+            } else {
+                number++;
+                number10++;
+                await Count.findByIdAndUpdate(dataCount._id, { count10: number10, count5: number5, countdata: number, countCard: numberCard });
+                if (number < arrData.length) {
+                    res.json({ code: 200, user: {} });
+                } else {
+                    res.json({ code: 200, user: {}, done: "true" });
+                }
+
+            }
         }
-        
+    } catch (err) {
+        res.json({ code: 200, user: {} });
     }
-    // call lại hàm
-    
 });
 
 
--- models/count.js
+++ models/count.js
@@ -4,6 +4,15 @@ const Schema = mongoose.Schema;
 const CountSchema = new Schema({
     countdata: {
         type: String
+    },
+    countCard: {
+        type: String
+    },
+    count5: {
+        type: String
+    },
+    count10: {
+        type: String
     }
 });
 
