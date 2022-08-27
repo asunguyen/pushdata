3e2dc7aecfe4d337a71e42c65297b8783a8699de
asunguyen <cntt510@gmail.com>
2022-08-27 20:45:02 +0700

    update excel

---
+++ test.js
@@ -0,0 +1,51 @@
+(function(cb) {
+    $model.sm('soquay', 'so1', '0', '--');
+$model.sm('soquay', 'so2', '0', '--');
+$model.sm('soquay', 'so3', '0', '--');
+$model.sm('soquay', 'so4', '0', '--');
+$model.sm('soquay', 'so5', '0', '--');
+$model.sm('soquay', 'so6', '0', '--');
+quay = 1;
+var w = 420;
+var h = 260;
+var width = $(window).outerWidth();
+var height = $(window).outerHeight();
+var tyleW = width / w;
+var tyleH = height / h;
+var wNew = tyleW * 33;
+var hNew = tyleH * 52;
+var fontSize = 13 * tyleW;
+$(".bt-1660772434960").css({ "left": parseFloat($(".bt-1660772434960").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660772434960").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660772434960").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660772434960").css("top")) * tyleH + "px" });
+$(".bt-1660772476344").css({ "left": parseFloat($(".bt-1660772476344").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660772476344").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660772476344").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660772476344").css("top")) * tyleH + "px" });
+$(".bt-1660772515305").css({ "cursor": "pointer", "left": parseFloat($(".bt-1660772515305").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660772515305").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660772515305").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660772515305").css("top")) * tyleH + "px" });
+$(".bt-1660772956648").css({ "left": parseFloat($(".bt-1660772956648").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660772956648").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660772956648").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660772956648").css("top")) * tyleH + "px" });
+$(".bt-1660772988007").css({ "left": parseFloat($(".bt-1660772988007").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660772988007").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660772988007").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660772988007").css("top")) * tyleH + "px" });
+$(".bt-1660773499559").css({"cursor": "pointer", "left": parseFloat($(".bt-1660773499559").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660773499559").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660773499559").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660773499559").css("top")) * tyleH + "px" });
+$(".bt-1660773366392").css({ "left": parseFloat($(".bt-1660773366392").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660773366392").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660773366392").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660773366392").css("top")) * tyleH + "px" });
+
+
+$(".bt-1660772613927").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660772613927").css("left")) * tyleW + "px", "width": wNew + "px", "height": hNew + "px", "top": parseFloat($(".bt-1660772613927").css("top")) * tyleH + "px" });
+$(".bt-1660772628954").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660772628954").css("left")) * tyleW + "px", "width": wNew + "px", "height": hNew + "px", "top": parseFloat($(".bt-1660772628954").css("top")) * tyleH + "px" });
+$(".bt-1660772680638").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660772680638").css("left")) * tyleW + "px", "width": wNew + "px", "height": hNew + "px", "top": parseFloat($(".bt-1660772680638").css("top")) * tyleH + "px" });
+$(".bt-1660772723496").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660772723496").css("left")) * tyleW + "px", "width": wNew + "px", "height": hNew + "px", "top": parseFloat($(".bt-1660772723496").css("top")) * tyleH + "px" });
+$(".bt-1660772741234").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660772741234").css("left")) * tyleW + "px", "width": wNew + "px", "height": hNew + "px", "top": parseFloat($(".bt-1660772741234").css("top")) * tyleH + "px" });
+$(".bt-1660772763024").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660772763024").css("left")) * tyleW + "px", "width": wNew + "px", "height": hNew + "px", "top": parseFloat($(".bt-1660772763024").css("top")) * tyleH + "px" });
+$(".bt-1660772777749").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660772777749").css("left")) * tyleW + "px", "width": wNew + "px", "height": hNew + "px", "top": parseFloat($(".bt-1660772777749").css("top")) * tyleH + "px" });
+
+$(".bt-1660773009300").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660773009300").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660773009300").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660773009300").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660773009300").css("top")) * tyleH + "px" });
+$(".bt-1660773131708").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660773131708").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660773131708").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660773131708").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660773131708").css("top")) * tyleH + "px" });
+$(".bt-1660773212746").css({ "font-size": fontSize + "px", "left": parseFloat($(".bt-1660773212746").css("left")) * tyleW + "px", "width": parseFloat($(".bt-1660773212746").css("width")) * tyleW + "px", "height": parseFloat($(".bt-1660773212746").css("height")) * tyleH + "px", "top": parseFloat($(".bt-1660773212746").css("top")) * tyleH + "px" });
+
+
+$(".bt-1660772613927 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660772628954 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660772680638 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660772723496 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660772741234 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660772763024 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660772777749 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660773009300 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660773131708 .block-content").css({ "font-size": fontSize + "px" });
+$(".bt-1660773212746 .block-content").css({ "font-size": fontSize + "px" });
+
+});
